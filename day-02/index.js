f8e0f5c47883d11afefa422d88c9fb8bc274b2a4
Max Schweikart <hello@maxschweik.art>
2020-12-10 22:40:07 +0100

    Implement day 02 part 2 solution.

--- day-02/index.js
+++ day-02/index.js
@@ -17,25 +17,42 @@ const validations = input.split('\n').map(line => {
     const match = line.match(/(\d+)-(\d+) ([a-z]): ([a-z]+)/);
     return {
         character: match[3],
-        minimum: parseInt(match[1]),
-        maximum: parseInt(match[2]),
+        position1: parseInt(match[1]),
+        position2: parseInt(match[2]),
         password: match[4]
     }
 });
 
 // validate passwords and count them
-let validPasswords = 0;
-validations.forEach(element => {
-    let thatCharacterCount = 0;
-    for (let i = 0; i < element.password.length; i++) {
-        if (element.password.charAt(i) == element.character) {
-            thatCharacterCount++;
+function partOne() {
+    let validPasswords = 0;
+    validations.forEach(element => {
+        let thatCharacterCount = 0;
+        for (let i = 0; i < element.password.length; i++) {
+            if (element.password.charAt(i) == element.character) {
+                thatCharacterCount++;
+            }
         }
-    }
+    
+        if (thatCharacterCount >= element.position1 && thatCharacterCount <= element.position2) {
+            validPasswords++;
+        }
+    });
+    return validPasswords;
+}
 
-    if (thatCharacterCount >= element.minimum && thatCharacterCount <= element.maximum) {
-        validPasswords++;
-    }
-});
+function partTwo() {
+    let validPasswords = 0;
+    validations.forEach(element => {
+        let isCharAtPos1 = element.password.charAt(element.position1 - 1) === element.character;
+        let isCharAtPos2 = element.password.charAt(element.position2 - 1) === element.character;
 
-console.log(`Found ${validPasswords} out of ${validations.length} valid passwords.`);
+        if (isCharAtPos1 != isCharAtPos2) { // != for booleans is XOR
+            validPasswords++;
+        }
+    });
+    return validPasswords;
+}
+
+console.log(`${partOne()} out of ${validations.length} passwords are valid with the sled rental place policy.`);
+console.log(`${partTwo()} out of ${validations.length} passwords are valid with the Official Toboggan Corporate policy.`);
