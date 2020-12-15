8dd2c18ab482e13c7c4f62f8caf302a84a4094e2
Max Schweikart <hello@maxschweik.art>
2020-12-15 23:54:42 +0100

    Implement day 09 part 2 solution.

--- day-09/index.js
+++ day-09/index.js
@@ -48,15 +48,50 @@ function validate(index) {
     }
 }
 
+function max(array) {
+    let max = array[0];
+    array.forEach(num => {
+        if (num > max) {
+            max = num;
+        }
+    });
+    return max;
+}
+
+function min(array) {
+    let min = array[0];
+    array.forEach(num => {
+        if (num < min) {
+            min = num;
+        }
+    });
+    return min;
+}
+
 function partOne() {
     for (let i = 25; i < numbers.length; i++) {
         if (!validate(i)) {
-            console.log(i);
-            console.log(numbers.slice(i - 25, i));
             return numbers[i];
         }
     }
     throw new Error('Could not find any number that is not the sum of the previous 25!');
 }
 
+function partTwo() {
+    const goal = partOne();
+
+    for (let lowerBoundIncl = 0; lowerBoundIncl < numbers.length; lowerBoundIncl++) {
+        let sum = numbers[lowerBoundIncl];
+        for (let upperBoundIncl = lowerBoundIncl + 1; upperBoundIncl < numbers.length && sum < goal; upperBoundIncl++) {
+            sum += numbers[upperBoundIncl];
+            if (sum === goal) {
+                const range = numbers.slice(lowerBoundIncl, upperBoundIncl + 1);
+                return min(range) + max(range); //success
+            }
+        }
+    }
+    return undefined; // failure
+}
+
 console.log(`Part one: ${partOne()} is the first number that is not the sum of the previous 25 numbers.`);
+console.log(`Part two: ${partTwo()} is the sum of the first and last number in the contiguous set that adds up to ${partOne()}.`);
