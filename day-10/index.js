f34d796432f62559b47e02e78f656a11c8ccb4cf
Max Schweikart <hello@maxschweik.art>
2020-12-18 21:33:27 +0100

    Implement day 10 part 2 solution.

--- day-10/index.js
+++ day-10/index.js
@@ -21,15 +21,15 @@ if (lines[lines.length - 1] === '') {
 
 const outputJoltages = lines.map(line => parseInt(line, 10));
 
+const joltageSteps = outputJoltages.sort((a, b) => a - b);
+
+// insert basis joltage amount
+joltageSteps.unshift(0);
+
+// append built-in joltage adapter
+joltageSteps.push(joltageSteps[joltageSteps.length - 1] + 3);
+
 function partOne() {
-    const joltageSteps = outputJoltages.sort((a, b) => a - b);
-
-    // insert basis joltage amount
-    joltageSteps.unshift(0);
-
-    // append built-in joltage adapter
-    joltageSteps.push(joltageSteps[joltageSteps.length - 1] + 3);
-
     // count joltage differences
     let diffOneCount = 0;
     let diffTwoCount = 0;
@@ -51,4 +51,27 @@ function partOne() {
     return diffOneCount * diffThreeCount;
 }
 
-console.log(`Part one: ${partOne()}`)
+function partTwo() {
+    const stepsWithData = joltageSteps.map(step => {
+        return {
+            joltage: step,
+            pathAmount: 0,
+        }
+    });
+    
+    stepsWithData[stepsWithData.length - 1].pathAmount = 1;
+
+    for (let i = stepsWithData.length - 1; i >= 0; i--) {
+        const adaptersToLookAt = stepsWithData.slice(i + 1, Math.min(stepsWithData.length, i + 4));
+        adaptersToLookAt.forEach(adapter => {
+            if ((adapter.joltage - stepsWithData[i].joltage) <= 3) {
+                stepsWithData[i].pathAmount += adapter.pathAmount;
+            }
+        });
+    }
+
+    return stepsWithData[0].pathAmount;
+}
+
+console.log(`Part one: ${partOne()}`);
+console.log(`Part two: ${partTwo()}`);
